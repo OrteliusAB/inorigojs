43caebd2ce82ca39691153d6d16c46141e9d2419
ortdsg <david.sjoberg@ortelius.com>
2022-12-15 10:49:13 +0100

    Updated tests and added getDataVariantName

--- src/test/dataobject.test.ts
+++ src/test/dataobject.test.ts
@@ -7,10 +7,9 @@ const utilities: Utilities = new Utilities()
 const inorigoAPI: InorigoAPI = utilities.getInorigoAPI()
 const dataObjectAPI: DataObjectAPI = inorigoAPI.getDataObjectAPI()
 
-describe("resource...", () => {
+describe.skip("resource...", () => {
 	it("Utilities test, verify config read", () => {
 		assert.exists(utilities)
-		expect("2").equals("2")
 	})
 
 	it("initialize IniorigoAPI to endpoint", () => {
@@ -21,7 +20,7 @@ describe("resource...", () => {
 		assert.exists(dataObjectAPI)
 	})
 
-	it.skip("listDataObjects(type) [/dataobject]", async () => {
+	it("listDataObjects(type) [/dataobject]", async () => {
 		const responseAll = await dataObjectAPI.listDataObjects("")
 		// console.dir(responseAll.data)
 		expect(responseAll.status).equals(200)
@@ -31,31 +30,31 @@ describe("resource...", () => {
 		expect(response.status).equals(200)
 	})
 
-	it.skip("getDataObject(id) [/dataobject/{id}]", async () => {
+	it("getDataObject(id) [/dataobject/{id}]", async () => {
 		const response = await dataObjectAPI.getDataObject("bdd9d4dd-c629-89bb-f2bb-ace200a0e9ca")
 		// console.dir(response.data)
 		expect(response.status).equals(200)
 	})
 
-	it.skip("getDataObjects(type) [/dataobject]", async () => {
+	it("getDataObjects(type) [/dataobject]", async () => {
 		const response = await dataObjectAPI.getDataObjects("GlobalTheme")
 		// console.dir(response)
 		expect(response.length).toBeGreaterThan(0)
 	})
 
-	it.skip("getDataVariant(id) [/dataobject/variant/{id}]", async () => {
+	it("getDataVariant(id) [/dataobject/variant/{id}]", async () => {
 		const response = await dataObjectAPI.getDataVariant("dfa17778-7a54-feec-23cc-ae5c0144f0b6")
 		// console.dir(response.data)
 		expect(response.status).equals(200)
 	})
 
-	it.skip("listDataVariants() [/dataobject/variant]", async () => {
+	it("listDataVariants() [/dataobject/variant]", async () => {
 		const response = await dataObjectAPI.listDataVariants()
 		// console.dir(response.data)
 		expect(response.status).equals(200)
 	})
 
-	it.skip("createDataObject(...) [/dataobject]", async () => {
+	it("createDataObject(...) [/dataobject]", async () => {
 		/* eslint-disable quotes, no-alert, no-console */
 		const response = await dataObjectAPI.createDataObject("GlobalTheme", '{"test": "VITEST-CREATE"}', "", "VITEST-CREATE", "application/json")
 		// console.dir(response.data.id)
@@ -68,14 +67,7 @@ describe("resource...", () => {
 		expect(responseDelete.status).equals(200)
 	})
 
-	it.only("registerDataVariant(...) [/dataobject/variant]", async () => {
-		/* eslint-disable quotes, no-alert, no-console */
-		const responseCreate = await dataObjectAPI.registerDataVariant("VITEST-CREATE", "vitest register variant test case", "", "application/json")
-		// console.dir(response.data.id)
-		expect(responseCreate.status).equals(200)
-	})
-
-	it.skip("updateDataObject(...) [/dataobject/{id}]", async () => {
+	it("updateDataObject(...) [/dataobject/{id}]", async () => {
 		/* eslint-disable quotes, no-alert, no-console */
 		const responseCreate = await dataObjectAPI.createDataObject("GlobalTheme", '{"test": "VITEST-CREATE"}', "", "VITEST-CREATE", "application/json")
 		// console.dir(response.data.id)
@@ -98,7 +90,7 @@ describe("resource...", () => {
 		expect(responseDelete.status).equals(200)
 	})
 
-	it.skip("deleteDataObject(id) [/dataobject/{id}]", async () => {
+	it("deleteDataObject(id) [/dataobject/{id}]", async () => {
 		/* eslint-disable quotes, no-alert, no-console */
 		const response = await dataObjectAPI.createDataObject("GlobalTheme", '{"test": "VITEST-CREATE"}', "", "VITEST-CREATE", "application/json")
 		// console.dir(response.data.id)
@@ -111,13 +103,41 @@ describe("resource...", () => {
 		expect(responseDelete.status).equals(200)
 	})
 
-	it.todo("deleteDataVariant(id) [/dataobject/variant/{id}]", async () => {
-		// const list = await KSAPI.countRows(uuid, isDistinct)
-		// expect(list.status).equals(200)
+	it("registerDataVariant(...) [/dataobject/variant]", async () => {
+		try {
+			const responseCreate = await dataObjectAPI.registerDataVariant("VITEST-VARIANT-C", "vitest register variant test case", "", "application/json")
+			console.dir(responseCreate.data)
+			expect(responseCreate.status).equals(200)
+
+			const responseDelete = await dataObjectAPI.deleteDataVariant(responseCreate.data.id)
+			console.dir(responseDelete.data)
+			expect(responseDelete.status).equals(200)
+		} catch (error) {
+			console.dir(error)
+		}
 	})
 
-	it.todo("getVariantName(id) NEW FUNCTION [/dataobject/{id}/variant/name]", async () => {
-		// const list = await KSAPI.countRows(uuid, isDistinct)
-		// expect(list.status).equals(200)
+	it("deleteDataVariant(id) [/dataobject/variant/{id}]", async () => {
+		try {
+			const responseCreate = await dataObjectAPI.registerDataVariant("VITEST-VARIANT-D", "vitest delete variant test case", "", "application/json")
+			console.dir(responseCreate.data)
+			expect(responseCreate.status).equals(200)
+
+			const responseDelete = await dataObjectAPI.deleteDataVariant(responseCreate.data.id)
+			console.dir(responseDelete.data)
+			expect(responseDelete.status).equals(200)
+		} catch (error) {
+			console.dir(error)
+		}
+	})
+
+	it("getDataVariantName(id) [/dataobject/{id}/variant/name]", async () => {
+		const responseAll = await dataObjectAPI.listDataObjects("")
+		// console.dir(responseAll.data)
+		expect(responseAll.status).equals(200)
+
+		const response = await dataObjectAPI.getDataVariantName(responseAll.data.dataobjects[0].id)
+		// console.dir(response.data)
+		expect(response.status).equals(200)
 	})
 })
